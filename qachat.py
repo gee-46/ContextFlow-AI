e50d574d99f6d6cb127cdc820a4a2b827453ad79
Gautam N Chipkar <gautamchipkar46@gmail.com>
2026-04-12 20:29:57 +0530

    Update qachat.py

--- qachat.py
+++ qachat.py
@@ -11,3 +11,8 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model=genai.GenerativeModel("gemini-pro") 
 chat = model.start_chat(history=[])
 def get_gemini_response(question):
+    
+    response=chat.send_message(question,stream=True)
+    return response
+
+##initialize our streamlit app
