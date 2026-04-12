2251e25f86cf5eb0b399fcde5070dd260d5066ae
Gautam N Chipkar <gautamchipkar46@gmail.com>
2026-04-12 20:28:31 +0530

    Update qachat.py

--- qachat.py
+++ qachat.py
@@ -4,3 +4,10 @@ load_dotenv() ## loading all the environment variables
 import streamlit as st
 import os
 import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
+def get_gemini_response(question):
